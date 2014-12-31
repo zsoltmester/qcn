e0c6db11f356adedfca6b4157cbffa33fcfb08ba
Mester Zsolt <zsolt.mester67@gmail.com>
2014-12-31 14:37:38 +0100

    Desktop activity change to valid skeleton

---
+++ app/src/main/java/zsoltmester/qcnotifications/desktop/ContactFragment.java
@@ -0,0 +1,18 @@
+package zsoltmester.qcnotifications.desktop;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import zsoltmester.qcnotifications.R;
+
+public class ContactFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_contact, container, false);
+	}
+
+}
